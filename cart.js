3e066a6f49f372e616363c4ef3237c547ae8e982
DEVA266 <devats266@gmail.com>
2025-02-13 22:43:52 +0530

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,10 @@
+let cartquantity = 0;
+            function quantity(cartquantity){
+                console.log("Cart Quantity : ",cartquantity);
+            }
+
+            function addtocart(cartquantity){
+              cartquantity = cartquantity + 1;
+              console.log("Cart Quantity : ",cartquantity);
+            }
+
