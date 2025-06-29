5d5b0a470a757656fb59e4626609df7e37ffbb83
DEVA266 <devats266@gail.com>
2025-06-29 14:03:39 +0530

    mm

--- cart.js
+++ cart.js
@@ -2,7 +2,6 @@ let cartquantity = 0;
             function quantity(cartquantity){
                 console.log("Cart Quantity : ",cartquantity);
             }
-
             function addtocart(cartquantity){
               cartquantity = cartquantity + 1;
               console.log("Cart Quantity : ",cartquantity);
