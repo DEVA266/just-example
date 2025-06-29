db523b0915ac68b8bef43de38ad85355b8ec1708
DEVA266 <devats266@gmail.com>
2025-06-29 14:10:51 +0530

    done

--- cart.js
+++ cart.js
@@ -2,6 +2,7 @@ let cartquantity = 0;
             function quantity(cartquantity){
                 console.log("Cart Quantity : ",cartquantity);
             }
+            
             function addtocart(cartquantity){
               cartquantity = cartquantity + 1;
               console.log("Cart Quantity : ",cartquantity);
